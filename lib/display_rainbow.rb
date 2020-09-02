05cf75fcd69fcecab6bcbea06912e189247e2902
Yat Sun Lam <yatsunlam25@gmail.com>
2020-09-02 04:58:39 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,4 +4,4 @@ def display_rainbow(colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indig
   Puts "#{letters[0]}: #{colors[0]}, #{letters[1]}: #{colors[1]}, #{letters[2]}: #{colors[2]}, #{letters[3]}: #{colors[3]}, #{letters[4]}: #{colors[4]}, #{letters[5]}: #{colors[5]}, #{letters[6]}: #{colors[6]}"
 end
 
-display_rainbow
+display_rainbow(colors)
